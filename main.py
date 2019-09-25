2c9589c0e96043d8624445d057b6a1408e98dacc
Kas Kroese <kas.kroese@geodan.nl>
2019-09-25 17:13:05 +0200

    Moved logger to a different function. Step names that make sense. Added logging level to output.

--- main.py
+++ main.py
@@ -2,29 +2,31 @@
 # Description:  Template for a script running a data update
 
 import logging
+from logger import create_logger
 from os import path, makedirs, sep
 from datetime import timedelta
 from time import time
 from pathlib import Path
 from collections import OrderedDict
-import dstlib.dstlib
-
+import dstlib
 
 
 SCRIPT_START = time()
 
 # define steps here
-steps = OrderedDict([("1", "step1.sql"),
-                     ("2", "something else")])
+steps = OrderedDict([("1", "create_schema.sql"),
+                     ("2", "something_else")])
 
 
 # parameters dummy
 # I want the log to be one level up. Keeps the git folder tidy (we need to discuss this)
 log_dir = Path(path.abspath(path.join(path.dirname(__file__), '..', 'log')).replace(sep, '/'))
+work_dir = Path(path.abspath(path.dirname(__file__)).replace(sep, '/'))
 version = 199001
 prep_steps = True
 schema = 'template'
 process = {"host": "etl-server03", "dbname": "process", "user": "geodbadmin", "password": "", "port": 5432}
+dbuser = 'geodb'
 
 
 def make_dir(directory: str) -> None:
@@ -33,7 +35,6 @@ def make_dir(directory: str) -> None:
     Directories: list of strings of the paths to be created as directories
     """
     if not path.exists(directory):
-       #logging.info(f'Creating folder {directory}')
        makedirs(directory)
 
 
@@ -42,42 +43,33 @@ def main() -> None:
     for directory in [log_dir]:
         make_dir(directory)
 
-    # Creating log file and -stream
-    logging.getLogger()
-    logging.basicConfig(
-                        level=logging.INFO,
-                        format='%(asctime)s %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S',
-                        handlers=[
-                            logging.FileHandler(f'{log_dir}/overall_log_{version}.log'),
-                            logging.StreamHandler()
-                        ])
-    logging.info(f'Starting conversion')
+    # create logger
+    create_logger(f'{log_dir}/overall_log_{version}.log')
 
+    logging.info(f'Starting conversion')
 
     # create database connection
     logging.info('Creating database connection')
-    connection = dstlib.dstlib.connect_databases(process)
+    connection = dstlib.connect_databases(process)
 
-    # defining queries
-    create_schema_query = f'''
-        DROP SCHEMA IF EXISTS {schema}_{version} CASCADE;
-        CREATE SCHEMA {schema}_{version} AUTHORIZATION {process['user']};
-        GRANT USAGE ON SCHEMA {schema}_{version} TO geodb;
-        '''
-
-    # running preparation steps
-    if prep_steps:
-        logging.info("Running preparation queries")
-        # executing queries
-        for query in [create_schema_query]:
-            dstlib.dstlib.execute_query(connection, query)
 
     # run steps
     for step in steps:
-        logging.info(f"running step {step}")
 
-        logging.info(f'Do something here')
+        logging.info(f"running step {step}")
+        if steps[step].endswith(".sql"):
+            logging.info(f'Executing query {steps[step]}')
+            step_query = (
+                open(f"{work_dir}\\sql\\{steps[step]}", encoding='utf-8-sig').read().format(version=version,
+                                                                                            schema=schema,
+                                                                                            dbadmin=process['user'],
+                                                                                            dbuser=dbuser))
+            dstlib.execute_query(connection, step_query)
+
+        else:
+            logging.info(f'Do something here')
+
+        logging.info(f'Finished running step {step}')
 
 
 if __name__ == '__main__':
---
+++ sql/create_schema.sql
@@ -0,0 +1,3 @@
+DROP SCHEMA IF EXISTS {schema}_{version} CASCADE;
+CREATE SCHEMA {schema}_{version} AUTHORIZATION {dbadmin};
+GRANT USAGE ON SCHEMA {schema}_{version} TO {dbuser};
