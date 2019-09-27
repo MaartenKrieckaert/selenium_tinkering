a3b8583e60832952776cada0dd8f6571a5faa2da
Kas Kroese <kas.kroese@geodan.nl>
2019-09-27 13:45:36 +0200

    Small addition to create schema query

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,3 +1,4 @@
 DROP SCHEMA IF EXISTS {schema}_{version} CASCADE;
 CREATE SCHEMA {schema}_{version} AUTHORIZATION {dbadmin};
 GRANT USAGE ON SCHEMA {schema}_{version} TO {dbuser};
+GRANT SELECT ON ALL TABLES IN SCHEMA {schema}_{version} TO {dbuser};
