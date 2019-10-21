0dfba4dfe38381ab16fe54a5ec131c43cc38f235
reinvantveer <rein.van.t.veer@geodan.nl>
2019-10-21 16:22:23 +0200

    test config reading

---
+++ test/test_config_parser.py
@@ -0,0 +1,22 @@
+import unittest
+
+from helper_functions.config_parser import import_config
+
+
+class TestConfigParser(unittest.TestCase):
+    def test_config_parsing_file_location(self) -> None:
+        with self.subTest('It raises on a file not found'):
+            with self.assertRaises(FileNotFoundError):
+                import_config('surely_not_found')
+
+        with self.subTest('It raises on trying to parse a template config'):
+            with self.assertRaises(ValueError):
+                import_config('config_template.yml')
+
+        with self.subTest('It successfully parses an example config'):
+            config = import_config('test_config.yml')
+            self.assertEqual(config['version'], '199001')
+
+
+if __name__ == '__main__':
+    unittest.main()
