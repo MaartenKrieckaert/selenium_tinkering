af316b09fbd6317a2c030c53d1ea0146d942af09
reinvantveer <rein.van.t.veer@geodan.nl>
2019-10-21 16:23:41 +0200

    improve typing, comments, fail on template loading and re-raise file not found error

--- helper_functions/config_parser.py
+++ helper_functions/config_parser.py
@@ -1,28 +1,32 @@
 # Title:        config_parser.py
 # Description:  Contains a function for loading a yaml configuration file
+from typing import Dict
 
-# import stuff
 import yaml
-import sys
 
 
-# function for importing the parameter file
-def import_config(config: str = 'config.yml') -> dict:
+def import_config(config: str = 'config.yml') -> Dict[str, str]:
     """
-    :param config: normally you shouldn't change this value. Rename the config file instead
+    Function for importing the parameter file
+    :param config: normally you shouldn't have to change this value.
+        Rename the config file from config_template.yml to config.yml instead
     :return: your parameters
     """
+
+    if config.endswith('config_template.yml'):
+        raise ValueError('Don\'t use the template config directly to load the configuration, '
+                         'create a copy of the template to config.yml instead.')
 
     try:
         # read config file
         with open(config) as f:
-            params = yaml.safe_load(f)
+            params = dict(yaml.safe_load(f))
 
         return params
 
     except FileNotFoundError:
         print(f'{config} cannot be found, have you renamed the config_template.yml to config.yml?')
-        sys.exit(1)
+        raise
 
 
 
